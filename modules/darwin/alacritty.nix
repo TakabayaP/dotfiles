5cb43d4c867987f56ad858907f96f50a54777906
Katsumi Kobayashi <takabayap@gmail.com>
2026-06-04 22:32:49 +0900

    Manage AeroSpace config as generated TOML

--- modules/darwin/alacritty.nix
+++ modules/darwin/alacritty.nix
@@ -3,6 +3,10 @@ let
   esc = builtins.fromJSON "\"\\u001b\"";
 in
 {
+  programs.alacritty.package = null;
+
+  programs.alacritty.settings.window.opacity = 0.8;
+
   programs.alacritty.settings.keyboard.bindings = [
     { key = "P"; mods = "Command"; chars = "${esc}[1;2P"; }
     { key = "F"; mods = "Command|Shift"; chars = "${esc}[1;2Q"; }
